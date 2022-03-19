fc1a92bdce2ae921e382b84124e3cec84649c9ad
jiezhuchen <jiezhuchen@tencent.com>
2022-03-19 19:26:59 +0800

    稍微优化一下代码结构

--- app/src/main/java/com/example/dctDemo/DCTTestDemo.java
+++ app/src/main/java/com/example/dctDemo/DCTTestDemo.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -14,7 +15,8 @@ import androidx.annotation.Nullable;
 public class DCTTestDemo extends Activity {
 //    private int mGrayPixels[] = {78,75,79,82,82,86,94,94,76,78,76,82,83,86,85,94,72,75,67,78,80,78,74,82,74,76,75,75,86,80,81,79,73,70,75,67,78,78,79,85,69,63,68,69,75,78,82,80,76,76,71,71,67,79,80,83,72,77,78,69,75,75,78,78};
 //    private int mGrayPixels[] = {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255};
-    private int mGrayPixels[] = {0,255,255,255,255,255,255,255,
+    private int mGrayPixels[] =
+        {0,255,255,255,255,255,255,255,
         255,0,255,255,255,255,255,0,
         255,255,0,255,255,255,0,255,
         255,255,255,0,255,0,255,255,
@@ -26,14 +28,8 @@ public class DCTTestDemo extends Activity {
     private double mDCTTransResult[][] = new double[8][8];
     private double mDCTReverseTransResult[][] = new double[8][8];
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ScrollView scrollViewContainer = new ScrollView(this);
-        LinearLayout totalView = new LinearLayout(this);
-        totalView.setOrientation(LinearLayout.VERTICAL);
-        totalView.setGravity(Gravity.CENTER);
-        //输入图像展示:
+    /**输入图像展示:**/
+    private void showInputSignalValues(LinearLayout totalView) {
         TextView inputSignalTitle = new TextView(this);
         inputSignalTitle.setText("输入图像展示");
         totalView.addView(inputSignalTitle);
@@ -52,7 +48,10 @@ public class DCTTestDemo extends Activity {
             }
             totalView.addView(hLinearLayout);
         }
-        //DCT输入信号展示：
+    }
+
+    /**DCT输入信号图像展示**/
+    private void showInputSignalImage(LinearLayout totalView) {
         BaseView inputSignalView = new BaseView(this);
         LinearLayout.LayoutParams inputSignalViewLayoutParams = new LinearLayout.LayoutParams(8 * 15, 8 * 15);
         inputSignalView.setLayoutParams(inputSignalViewLayoutParams);
@@ -62,8 +61,10 @@ public class DCTTestDemo extends Activity {
             }
         }
         totalView.addView(inputSignalView);
+    }
 
-        //DCT基展示
+    /**根据传入的u,v值(0,0)到(7,7)得到8x8的DCT基，并在UI中展示。计算后的DCT基保存在DCTBaseView中**/
+    private void createDCTBaseMatrixAndShowIt(LinearLayout totalView) {
         TextView DCTBaseMatrixTitle = new TextView(this);
         DCTBaseMatrixTitle.setText("DCT变换使用的基表");
         totalView.addView(DCTBaseMatrixTitle);
@@ -81,21 +82,26 @@ public class DCTTestDemo extends Activity {
             }
             totalView.addView(hLinearLayout);
         }
+    }
 
-        //DCT变换后的频域矩阵，不同uv坐标的dct基的x,y的值 * 原信号的x,y的值
+    /**对输入的8*8矩阵信号，分别乘以从最低频（u,v为(0,0）处的DCT基）到最高频的DCT（u,v为(u,v）处的DCT基）基矩阵并求和，
+     * 求和结果进行亮度量化后，放到输出矩阵对应坐标(u,v)处**/
+    private void signalToDCTSignalTrans(int inputMatrix[], double outputMatrix[][]) {
         for (int u = 0; u < 8; u ++) {
             for (int v = 0; v < 8; v ++) {
                 double base = 0;
                 for (int x = 0; x < 8; x ++) {
                     for (int y = 0; y < 8; y ++) {
-                        base += mGrayPixels[y * 8 + x] * mDCTBaseMatrix[u][v].getSignalDCTBaseVal(x, y);
+                        base += inputMatrix[y * 8 + x] * mDCTBaseMatrix[u][v].getSignalDCTBaseVal(x, y);
                     }
                 }
-                mDCTTransResult[u][v] = 1f / 4f * base / Constant.DCT_BRIGHTNESS_TRANS_TABLE[u * 8 + v];
+                outputMatrix[u][v] = 1f / 4f * base / Constant.DCT_BRIGHTNESS_TRANS_TABLE[u * 8 + v];
             }
         }
+    }
 
-        //DCT变换后的画面呈现
+    /**以图形方式显示被信号通过DCT处理后的频域信号的模样**/
+    private void showDCTTransResult(LinearLayout totalView) {
         TextView DCTTitle = new TextView(this);
         DCTTitle.setText("DCT变换 + 亮度表量化后的结果：");
         totalView.addView(DCTTitle);
@@ -114,8 +120,10 @@ public class DCTTestDemo extends Activity {
             }
             totalView.addView(hLinearLayout);
         }
+    }
 
-        //DCT逆变换：
+    /**IDCT变换，把DCT变换后的频域信号重新转化为时域信号**/
+    private void DCTSignalInverseDCTToSignal() {
         for (int y = 0; y < 8; y ++) {
             for (int x = 0; x < 8; x ++) {
                 double base = 0;
@@ -128,8 +136,10 @@ public class DCTTestDemo extends Activity {
                 mDCTReverseTransResult[x][y] = 1f / 4f * base;
             }
         }
+    }
 
-        //DCT逆变换数据呈现
+    /**把IDCT变换后的频域数据视觉化**/
+    private void DCTInverseDataShow(LinearLayout totalView) {
         TextView reverseDCTTitle = new TextView(this);
         reverseDCTTitle.setText("逆亮度量化 + DCT逆变换");
         totalView.addView(reverseDCTTitle);
@@ -148,8 +158,9 @@ public class DCTTestDemo extends Activity {
             }
             totalView.addView(hLinearLayout);
         }
+    }
 
-        //DCT逆变换图像呈现
+    private void DCTInverseImageShow(LinearLayout totalView) {
         BaseView reverseSignalView = new BaseView(this);
         LinearLayout.LayoutParams reverseSignalViewLp = new LinearLayout.LayoutParams(8 * 15, 8 * 15);
         reverseSignalView.setLayoutParams(reverseSignalViewLp);
@@ -160,20 +171,32 @@ public class DCTTestDemo extends Activity {
             }
         }
         totalView.addView(reverseSignalView);
-
+    }
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ScrollView scrollViewContainer = new ScrollView(this);
+        LinearLayout totalView = new LinearLayout(this);
+        totalView.setOrientation(LinearLayout.VERTICAL);
+        totalView.setGravity(Gravity.CENTER);
+        //输入图像展示:
+        showInputSignalValues(totalView);
+        //DCT输入信号展示：
+        showInputSignalImage(totalView);
+        //DCT基展示
+        createDCTBaseMatrixAndShowIt(totalView);
+        //DCT变换后的频域矩阵，不同uv坐标的dct基的x,y的值 * 原信号的x,y的值
+        signalToDCTSignalTrans(mGrayPixels, mDCTTransResult);
+        //DCT变换后的画面呈现
+        showDCTTransResult(totalView);
+        //DCT逆变换：
+        DCTSignalInverseDCTToSignal();
+        //DCT逆变换数据呈现
+        DCTInverseDataShow(totalView);
+        //DCT逆变换图像呈现
+        DCTInverseImageShow(totalView);
         //添加到滚动窗
         scrollViewContainer.addView(totalView);
-
         setContentView(scrollViewContainer);
-
-
-
-
-
-
-
-
-
-
     }
 }
