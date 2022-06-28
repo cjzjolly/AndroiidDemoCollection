d4a21c6beebe54e51bd8f59ac11ad4b3c498f510
chenjiezhu@wps.cn <chenjiezhu@wps.cn>
2022-06-28 17:17:45 +0800

    实现白板瓦片的滑动和缩放

--- app/src/main/java/com/example/whiteboard/MapUnit.java
+++ app/src/main/java/com/example/whiteboard/MapUnit.java
@@ -1,54 +1,124 @@
 package com.example.whiteboard;
 
-import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.util.AttributeSet;
+import android.graphics.PointF;
+import android.graphics.RectF;
 import android.util.Log;
-import android.view.View;
+
 import java.util.Random;
 
 /**
  * Created by cjz on 2019/4/30.
  */
 
-public class MapUnit extends View {
+public class MapUnit {
+    private Paint mPaint;
+    private RectF mMapRange;
+    private int mUnitXY[] = new int[2];
+    private float mScale = 1f;
 
-    public MapUnit(Context context) {
-        super(context);
-    }
-
-    public MapUnit(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public MapUnit(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
-
-    @Override
-    protected void onDraw(Canvas canvas) {
+    public MapUnit(RectF range) {
+        mMapRange = new RectF(range);
         Random random = new Random();
+        mPaint = new Paint();
+        mPaint.setStrokeWidth(8f);
+        mPaint.setColor((0xFF000000 | (random.nextInt(255) & 0xFF) << 16 | (random.nextInt(255) & 0xFF) << 8 | (random.nextInt(255) & 0xFF)));
+        mPaint.setStyle(Paint.Style.FILL);
+    }
+
+    public void offset(int dx, int dy) {
+        if (null == mMapRange) {
+            return;
+        }
+        mMapRange.offset(dx, dy);
+    }
+
+    public void scale(float scale) {
+        if (null == mMapRange) {
+            return;
+        }
+        Matrix matrix = new Matrix();
+        matrix.setScale(scale, scale);
+        matrix.mapRect(mMapRange);
+        mScale *= scale;
+    }
+
+    public float getScale() {
+        return mScale;
+    }
+
+    public PointF getCenter() {
+        if (null == mMapRange) {
+            return null;
+        }
+        return new PointF(mMapRange.centerX(), mMapRange.centerY());
+    }
+
+    public PointF getXY() {
+        if (null == mMapRange) {
+            return null;
+        }
+        return new PointF(mMapRange.left, mMapRange.top);
+    }
+
+    public void setX(float x) {
+        if (null == mMapRange) {
+            return;
+        }
+        mMapRange.offset(x - mMapRange.left, 0);
+    }
+
+    public void setY(float y) {
+        if (null == mMapRange) {
+            return;
+        }
+        mMapRange.offset(0, y - mMapRange.top);
+    }
+
+    public float getWidth() {
+        if (null == mMapRange) {
+            return 0;
+        }
+        return mMapRange.width();
+    }
+
+    public float getHeight() {
+        if (null == mMapRange) {
+            return 0;
+        }
+        return mMapRange.height();
+    }
+
+
+    public void setTag(int unitXY[]) {
+        mUnitXY[0] = unitXY[0];
+        mUnitXY[1] = unitXY[1];
+    }
+
+    public int[] getTag() {
+        return mUnitXY;
+    }
+
+    protected void onDraw(Canvas canvas) {
+        if (null == mMapRange) {
+            return;
+        }
         Log.i("onDraw", hashCode() + "");
-        super.onDraw(canvas);
-        Paint paint = new Paint();
-        paint.setStrokeWidth(8f);
-        paint.setColor((0xFF000000 | (random.nextInt(255) & 0xFF) << 16 | (random.nextInt(255) & 0xFF) << 8 | (random.nextInt(255) & 0xFF)));
-        paint.setStyle(Paint.Style.FILL);
         //绘制随机色背景
-        canvas.drawRect(0, 0, getWidth(), getHeight(), paint);
+        canvas.drawRect(mMapRange, mPaint);
         Paint paintPen = new Paint();
         paintPen.setStrokeWidth(8f);
         paintPen.setStyle(Paint.Style.FILL);
         paintPen.setColor(Color.BLACK);
-        paintPen.setTextSize(120f);
+        paintPen.setTextSize(20f);
         paintPen.setAntiAlias(true);
         //绘制自己是第几列第几行的单元
-        if(getTag() != null) {
-            int position[] = (int[]) getTag();
-            canvas.drawText(String.format("UnitX: %d, UnitY: %d", position[0], position[1]),  getWidth() / 2 - 100, getHeight() / 2, paintPen);
+        if(mUnitXY != null) {
+            int position[] = mUnitXY;
+            canvas.drawText(String.format("UnitX: %d, UnitY: %d", position[0], position[1]),  mMapRange.centerX() - 40, mMapRange.centerY(), paintPen);
         }
     }
 }
--- app/src/main/java/com/example/whiteboard/Whiteboard.java
+++ app/src/main/java/com/example/whiteboard/Whiteboard.java
@@ -1,17 +1,14 @@
 package com.example.whiteboard;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.PointF;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -19,22 +16,21 @@ import java.util.concurrent.LinkedBlockingQueue;
  * Created by cjz on 2019/4/30.
  */
 
-public class Whiteboard extends FrameLayout {
-
+public class Whiteboard extends View {
     private PointF currentCenter = new PointF();
     private PointF prevCurrentCenter = null;
     private float prevDistance = Float.MIN_VALUE;
     private float totalScale = 1f;
+    private int mWidth = -1;
+    private int mHeight = -1;
     private float dx = 0, dy = 0;
     /**缩放比例上限**/
-    private final float MAX_SCALE = 2f;
+    private final float MAX_SCALE = 4f;
     /**缩放比例下限**/
     private final float MIN_SCALE = 0.5f;
-    /**单元格矩阵长宽均有多少个单元**/
-    private final int MATRIX_LENGTH = 4;
+    private final int MATRIX_LENGTH = 8;
     /**单元格表**/
     private MapUnit mapUnitMatrix[][] = new MapUnit[MATRIX_LENGTH][MATRIX_LENGTH];
-    private boolean initFinished = false;
 
     /*** 触摸点点距队列**/
     private Queue<Float> touchDistanceQueue = new LinkedBlockingQueue<>();
@@ -50,33 +46,30 @@ public class Whiteboard extends FrameLayout {
 
     public Whiteboard(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        if(!initFinished){
-            createView();
-            initFinished = true;
+        int w = MeasureSpec.getSize(widthMeasureSpec);
+        int h = MeasureSpec.getSize(heightMeasureSpec);
+        if (w != mWidth || h != mHeight) { //已经onMeasuer过一次，除非界面大小改动否则不重新初始化view
+            mWidth = w;
+            mHeight = h;
+            createUnits();
         }
     }
 
-    private void createView() {
+    private void createUnits() {
+        int unitWidth = (int) (mWidth / MATRIX_LENGTH / MIN_SCALE);
+        int unitHeight = (int) (mHeight / MATRIX_LENGTH / MIN_SCALE);
         for(int yPos = 0; yPos < MATRIX_LENGTH; yPos++) {
             for (int xPos = 0; xPos < MATRIX_LENGTH; xPos++) {
-                MapUnit mapUnit = new MapUnit(getContext());
-//                mapUnit.setImageResource(R.mipmap.ic_launcher);
-                mapUnit.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                RectF unitRange = new RectF(0, 0, unitWidth, unitHeight);
+                unitRange.offset(xPos * unitWidth, yPos * unitHeight);
+                MapUnit mapUnit = new MapUnit(unitRange);
                 mapUnit.setTag(new int[]{xPos, yPos});
-                addView(mapUnit);
                 mapUnitMatrix[xPos][yPos] = mapUnit;
-            }
-        }
-        for(int yPos = 0; yPos < MATRIX_LENGTH; yPos++) {
-            for (int xPos = 0; xPos < MATRIX_LENGTH; xPos++) {
-                mapUnitMatrix[xPos][yPos].setX(xPos * getMeasuredWidth());
-                mapUnitMatrix[xPos][yPos].setY(yPos * getMeasuredHeight());
             }
         }
     }
@@ -158,9 +151,9 @@ public class Whiteboard extends FrameLayout {
                 touchDistanceQueue.clear();
                 break;
         }
+        invalidate();
         return true;
     }
-
 
     /**
      * 缩放函数
@@ -172,21 +165,22 @@ public class Whiteboard extends FrameLayout {
         totalScale *= scale;
         for(int yPos = 0; yPos < MATRIX_LENGTH; yPos++) {
             for (int xPos = 0; xPos < MATRIX_LENGTH; xPos++) {
-                View view = mapUnitMatrix[xPos][yPos];
+                MapUnit mapUnit = mapUnitMatrix[xPos][yPos];
                 //以本View中心点为缩放中心缩放
-                view.setScaleX(view.getScaleX() * scale);
-                view.setScaleY(view.getScaleY() * scale);
+//                view.setScaleX(view.getScaleX() * scale);
+//                view.setScaleY(view.getScaleY() * scale);
+                mapUnit.scale(scale);
                 //求本view中心点在屏幕中的坐标
-                float centerX = view.getX() + view.getWidth() / 2;
-                float centerY = view.getY() + view.getHeight() / 2;
+//                float centerX = view.getX() + view.getWidth() / 2;
+//                float centerY = view.getY() + view.getHeight() / 2;
                 /**向缩放中心靠拢，例如缩放为原来的80%，那么缩放中心x到view中心x的距离则为0.8*(缩放中心x - view中心x),
                  * 那么view的x距离屏幕左边框的距离则 为   view中心x + (1 - 0.8) * (缩放x - view中心x)  ****/
-                float centerXAfterScale = centerX + (px - centerX) * (1 - scale); //view中心向缩放中心聚拢或扩散
-                float centerYAfterScale = centerY + (py - centerY) * (1 - scale);
-                view.setX(centerXAfterScale - view.getWidth() / 2); //setXY是set左上角的x,y，所以view中心点要减去宽度/高度的一般来重新得到应该去的左上角坐标
-                view.setY(centerYAfterScale - view.getHeight() / 2);
+//                float centerXAfterScale = centerX + (px - centerX) * (1 - scale); //view中心向缩放中心聚拢或扩散
+//                float centerYAfterScale = centerY + (py - centerY) * (1 - scale);
+//                view.setX(centerXAfterScale - view.getWidth() / 2); //setXY是set左上角的x,y，所以view中心点要减去宽度/高度的一般来重新得到应该去的左上角坐标
+//                view.setY(centerYAfterScale - view.getHeight() / 2);
 //            viewFind(view, this.scale);
-                Log.i("View" + view.hashCode() + "的信息", String.format("长度:%d, 宽度:%d, 坐标x:%f, 坐标y:%f", view.getWidth(), view.getHeight(), view.getX(), view.getY()));
+//                Log.i("View" + view.hashCode() + "的信息", String.format("长度:%d, 宽度:%d, 坐标x:%f, 坐标y:%f", view.getWidth(), view.getHeight(), view.getX(), view.getY()));
             }
         }
         Log.i("缩放", String.format("百分比：%f", totalScale));
@@ -200,32 +194,33 @@ public class Whiteboard extends FrameLayout {
         dy += distanceY;
         for(int yPos = 0; yPos < MATRIX_LENGTH; yPos++) {
             for (int xPos = 0; xPos < MATRIX_LENGTH; xPos++) {
-                View view = mapUnitMatrix[xPos][yPos];
-                view.setX(view.getX() + (distanceX));
-                view.setY(view.getY() + (distanceY));
+                MapUnit view = mapUnitMatrix[xPos][yPos];
+                view.offset((int) distanceX, (int) distanceY);
             }
         }
         //x轴,y轴要分开两个循环处理，否则会引发混乱
         for(int yPos = 0; yPos < MATRIX_LENGTH; yPos++) {
             for (int xPos = 0; xPos < MATRIX_LENGTH; xPos++) {
-                View view = mapUnitMatrix[xPos][yPos];
+                MapUnit view = mapUnitMatrix[xPos][yPos];
                 //移除去的部分添加到未显示的部分的末尾
-                if(view.getX()  + (1 - view.getScaleX()) / 2 * view.getWidth() + view.getWidth() * view.getScaleX() < 0  && getWidth() > 0) { //单元格溢出到了屏幕左边，移动到当前对应行最右边
-                    if(xPos == 0) {
+                if (view.getXY().x + view.getWidth() < 0 && mWidth > 0) { //单元格溢出到了屏幕左边，移动到当前对应行最右边
+                    if (xPos == 0) {
                         //重设位置
-                        view.setX(mapUnitMatrix[MATRIX_LENGTH - 1][yPos].getX() + mapUnitMatrix[MATRIX_LENGTH - 1][yPos].getWidth() * mapUnitMatrix[MATRIX_LENGTH - 1][yPos].getScaleX());
+                        view.setX(mapUnitMatrix[MATRIX_LENGTH - 1][yPos].getXY().x + mapUnitMatrix[MATRIX_LENGTH - 1][yPos].getWidth());
+                        view.setTag(new int[] {mapUnitMatrix[MATRIX_LENGTH - 1][yPos].getTag()[0] + 1, view.getTag()[1]});
                         for (int i = xPos; i < MATRIX_LENGTH - 1; i++) {
                             mapUnitMatrix[i][yPos] = mapUnitMatrix[i + 1][yPos];
                         }
                         mapUnitMatrix[MATRIX_LENGTH - 1][yPos] = (MapUnit) view;
                     }
                 }
-                else if (view.getX() + (1 - view.getScaleX()) / 2 * view.getWidth() > getWidth() && getWidth() > 0) {
-                    if(xPos == MATRIX_LENGTH - 1){ //因为初始化时显示的Unit是最左上角的Unit，有可能导致非最后一列的内容被平移，这违反自动补充的逻辑，会出bug，所以要加判断
+                else if (view.getXY().x > mWidth && mWidth > 0) {
+                    if (xPos == MATRIX_LENGTH - 1) { //因为初始化时显示的Unit是最左上角的Unit，有可能导致非最后一列的内容被平移，这违反自动补充的逻辑，会出bug，所以要加判断
                         //重设位置(设置和最后一个的左上角坐标直接重合（setx用于设定左上角坐标），再减去控件宽度*缩放量使得目标控件右上角和最后一个控件左上角对齐)
-                        view.setX(mapUnitMatrix[0][yPos].getX() - mapUnitMatrix[0][yPos].getWidth() * mapUnitMatrix[0][yPos].getScaleX());
+                        view.setX(mapUnitMatrix[0][yPos].getXY().x - mapUnitMatrix[0][yPos].getWidth());
+                        view.setTag(new int[] {mapUnitMatrix[0][yPos].getTag()[0] - 1, view.getTag()[1]});
                         MapUnit temp = mapUnitMatrix[MATRIX_LENGTH - 1][yPos];
-                        for(int i = MATRIX_LENGTH - 1; i > 0 ; i--){
+                        for (int i = MATRIX_LENGTH - 1; i > 0 ; i--) {
                             mapUnitMatrix[i][yPos] = mapUnitMatrix[i - 1][yPos];
                         }
                         mapUnitMatrix[0][yPos] = temp;
@@ -235,22 +230,24 @@ public class Whiteboard extends FrameLayout {
         }
         for(int yPos = 0; yPos < MATRIX_LENGTH; yPos++) {
             for (int xPos = 0; xPos < MATRIX_LENGTH; xPos++) {
-                View view = mapUnitMatrix[xPos][yPos];
-                if (view.getY() + (1 - view.getScaleY()) / 2 * view.getHeight() + view.getHeight() * view.getScaleY() < 0 && getHeight() > 0) {
+                MapUnit view = mapUnitMatrix[xPos][yPos];
+                if (view.getXY().y + view.getHeight() < 0 && mHeight > 0) {
                     if (yPos == 0) {
                         //重设位置
-                        view.setY(mapUnitMatrix[xPos][MATRIX_LENGTH - 1].getY() + mapUnitMatrix[xPos][MATRIX_LENGTH - 1].getHeight() * mapUnitMatrix[xPos][MATRIX_LENGTH - 1].getScaleY());
+                        view.setY(mapUnitMatrix[xPos][MATRIX_LENGTH - 1].getXY().y + mapUnitMatrix[xPos][MATRIX_LENGTH - 1].getHeight());
+                        view.setTag(new int[] {view.getTag()[0], mapUnitMatrix[xPos][MATRIX_LENGTH - 1].getTag()[1] + 1});
                         for (int i = yPos; i < MATRIX_LENGTH - 1; i++) {
                             mapUnitMatrix[xPos][i] = mapUnitMatrix[xPos][i + 1];
                         }
                         mapUnitMatrix[xPos][MATRIX_LENGTH - 1] = (MapUnit) view;
                     }
                 }
-                else if (view.getY() + (1 - view.getScaleY()) / 2 * view.getHeight() > getHeight() && getHeight() > 0) {
+                else if (view.getXY().y > mHeight && mHeight > 0) {
                     if (yPos == MATRIX_LENGTH - 1) {
                         //Log.i("越位", "到了屏幕下边界");
                         //重设位置(设置和最后一个的左上角坐标直接重合（setx用于设定左上角坐标），再减去控件宽度*缩放量使得目标控件右上角和最后一个控件左上角对齐)
-                        view.setY(mapUnitMatrix[xPos][0].getY() - view.getHeight() * view.getScaleY());
+                        view.setY(mapUnitMatrix[xPos][0].getXY().y - mapUnitMatrix[xPos][0].getHeight());
+                        view.setTag(new int[] {view.getTag()[0], mapUnitMatrix[xPos][0].getTag()[1] - 1});
                         MapUnit temp = mapUnitMatrix[xPos][MATRIX_LENGTH - 1];
                         for (int i = MATRIX_LENGTH - 1; i > 0; i--) {
                             mapUnitMatrix[xPos][i] = mapUnitMatrix[xPos][i - 1];
@@ -261,7 +258,22 @@ public class Whiteboard extends FrameLayout {
             }
         }
         Log.i("移动", String.format("x位移：%f， y位移：%f", distanceX, distanceY));
-//        invalidate();
+        invalidate();
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (null == mapUnitMatrix) {
+            return;
+        }
+        for(int yPos = 0; yPos < MATRIX_LENGTH; yPos++) {
+            for (int xPos = 0; xPos < MATRIX_LENGTH; xPos++) {
+                MapUnit mapUnit = mapUnitMatrix[xPos][yPos];
+                if (mapUnit == null) {
+                    continue;
+                }
+                mapUnit.onDraw(canvas);
+            }
+        }
+    }
 }
