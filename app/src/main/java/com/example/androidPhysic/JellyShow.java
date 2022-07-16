30e5d270be4f548c13ca031543957a2a397093fb
DESKTOP-IGVT5MR\HP <492627801@qq.com>
2022-07-16 21:06:21 +0800

    暂存进度

---
+++ app/src/main/java/com/example/androidPhysic/JellyShow.java
@@ -0,0 +1,20 @@
+package com.example.androidPhysic;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+/**todo 试图使用canvas实现物理效果**/
+public class JellyShow extends SurfaceView {
+    public JellyShow(Context context) {
+        super(context);
+    }
+
+    public JellyShow(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public JellyShow(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
