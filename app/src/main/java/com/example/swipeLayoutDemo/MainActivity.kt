7eec19b02b63ca49374f4b45ea506be88c60c572
chenjiezhu <chenjiezhu@wps.cn>
2024-03-05 22:28:23 +0800

    暂存当前进度

--- app/src/main/java/com/example/swipeLayoutDemo/MainActivity.kt
+++ app/src/main/java/com/example/swipeLayoutDemo/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.swipeLayoutDemo
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.example.piccut.R
 
 class MainActivity : Activity() {
@@ -12,5 +16,9 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.swipe_demo)
         mSr = findViewById(R.id.sr_test)
+        val v = View(this)
+        v.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 300)
+        v.setBackgroundColor(Color.RED)
+        mSr?.setTipsView(v)
     }
 }
--- app/src/main/java/com/example/swipeLayoutDemo/SwipeRefreshLayout.kt
+++ app/src/main/java/com/example/swipeLayoutDemo/SwipeRefreshLayout.kt
@@ -1,48 +1,133 @@
 package com.example.swipeLayoutDemo
 
+import android.animation.Animator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
+import android.view.View
 import android.widget.FrameLayout
 import com.example.photoCutter.MeasurelUtils
+import java.util.*
+import kotlin.collections.HashMap
 
 class SwipeRefreshLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : FrameLayout(context, attrs) {
+    interface SwipeCallBack {
+        fun callRefresh()
+    }
+
+    private var mWidth = 0
+    private var mHeight = 0
     private var mAnimator: ValueAnimator? = null
     private val OND_DP = MeasurelUtils.convertDpToPixel(1f, context)
     private var mPrevY = 0f
     private var mMaxOffsetY = 100 * OND_DP
+    private var mSwipeCallBack: SwipeCallBack? = null
+    /**下拉提示**/
+    private var mTipsView: View? = null
+    private var mOffsetY = 0f
+    private val childYList = HashMap<View, Float>()
+
+    fun setTipsView(view: View) {
+        mTipsView = view
+        addView(mTipsView, 0)
+        view.visibility = INVISIBLE
+    }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         Log.e("cjztest", "MotionEvent:${event}")
         when (event?.action) {
             MotionEvent.ACTION_MOVE -> {
+                mTipsView?.visibility = VISIBLE
                 val deltaY = event?.rawY - mPrevY
-                if (y + deltaY > 0 && y + deltaY < mMaxOffsetY) {
-                    y += deltaY
-                } else {
-
-                }
+                setViewYAxis(deltaY)
             }
-            //todo 使用动画回弹
+            //使用动画回弹
             MotionEvent.ACTION_UP -> {
                 mAnimator?.cancel()
-                mAnimator = ValueAnimator.ofFloat(y, 0f)
+                mAnimator = ValueAnimator.ofFloat(mOffsetY, 0f)
                 mAnimator?.run {
+                    var prevVal = mOffsetY
                     addUpdateListener { animation ->
                         val currentY = animation?.animatedValue as Float
-                        y = currentY
+                        val deltaY = currentY - prevVal
+                        setViewYAxis(deltaY)
+                        prevVal = currentY
                         Log.e("cjztest", "CurrentY:${currentY}")
                     }
+                    addListener(object : Animator.AnimatorListener {
+                        override fun onAnimationStart(animation: Animator?) {
+
+                        }
+
+                        override fun onAnimationEnd(animation: Animator?) {
+                            for (i in 0 until childCount) {
+                                val v = getChildAt(i)
+                                childYList[v]?.run {
+                                    v.y = this@run
+                                }
+                            }
+                            mTipsView?.visibility = INVISIBLE
+                        }
+
+                        override fun onAnimationCancel(animation: Animator?) {
+                            onAnimationEnd(animation)
+                        }
+
+                        override fun onAnimationRepeat(animation: Animator?) {
+
+                        }
+
+                    })
                     duration = 300
                     start()
+                    callRefresh()
                 }
             }
         }
         mPrevY = event?.rawY!!
         return true
+    }
+
+    private fun callRefresh() {
+        mSwipeCallBack?.callRefresh()
+    }
+
+    private fun setViewYAxis(deltaY: Float) {
+        if (mOffsetY + deltaY < 0 || mOffsetY + deltaY > mTipsView?.bottom!!) {
+            return
+        }
+        mOffsetY += deltaY
+        for (i in 0 until childCount) {
+            val v = getChildAt(i)
+            if (v === mTipsView) {
+                continue
+            }
+            v.y += deltaY
+        }
+    }
+
+    private fun recordYData() {
+        for (i in 0 until childCount) {
+            val v = getChildAt(i)
+            if (v === mTipsView) {
+                continue
+            }
+            childYList[v] = v.y
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val w = MeasureSpec.getSize(widthMeasureSpec)
+        val h = MeasureSpec.getSize(heightMeasureSpec)
+        if (w != mWidth || h != mHeight) { //已经onMeasuer过一次，除非界面大小改动否则不重新初始化view
+            mWidth = MeasureSpec.getSize(widthMeasureSpec)
+            mHeight = MeasureSpec.getSize(heightMeasureSpec)
+            recordYData()
+        }
     }
 }
